14f548947c9230be821551469d77aa096dd5743e
douryne <douryne.1st@gmail.com>
2023-10-08 23:54:49 +0400

    chore: auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import AutoImport from "unplugin-auto-import/vite";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [
+      qwikCity(),
+      qwikVite(),
+      tsconfigPaths(),
+      AutoImport({
+        imports: [{ "@builder.io/qwik": ["component$"] }],
+        dts: "./src/auto-imports.d.ts"
+      })
+    ],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
